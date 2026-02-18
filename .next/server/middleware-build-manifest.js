297760b9d0efb7cae21590c75f4eaf4fa2f0ede9
Daniel-wambua <daniel.wambua@gmail.com>
2026-02-18 15:50:21 +0300

    for the phone use iphone

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -8,10 +8,10 @@ globalThis.__BUILD_MANIFEST = {
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/282dd41d755f57df.js",
+    "static/chunks/4b9eae0c8dc7e975.js",
     "static/chunks/f2f58a7e93290fbb.js",
-    "static/chunks/74801f56d8636cf9.js",
-    "static/chunks/turbopack-784a6dc00802650d.js"
+    "static/chunks/a90b6d11afe742e2.js",
+    "static/chunks/turbopack-1e9ff4ec3e6618de.js"
   ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
